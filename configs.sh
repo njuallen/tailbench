ce62686c76d04d2dbfae59341db3b96e8f29c67e
Allen <njuallen@foxmail.com>
2020-03-14 17:55:14 +0800

    Modified local settings:path, jdk, etc. Failed to compile moses, silo, specjbb, sphinx, xapian.

--- configs.sh
+++ configs.sh
@@ -1,9 +1,9 @@
 # Set this to point to the top level of the TailBench data directory
-DATA_ROOT=/topleveldatadir
+DATA_ROOT=../tailbench.inputs
 
 # Set this to point to the top level installation directory of the Java
 # Development Kit. Only needed for Specjbb
- JDK_PATH=/pathtojdk
+JDK_PATH=$JAVA_HOME
 
 # This location is used by applications to store scratch data during execution.
-SCRATCH_DIR=/pathtoscratch
+SCRATCH_DIR=../tailbench.scratch
