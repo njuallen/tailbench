6f2311f05d2bd2bb84366f0125983f53756b4292
Allen <njuallen@foxmail.com>
2020-03-16 10:53:28 +0800

    将xapian编译通过，看来必须得先把xapian core手动编译过了，才能编译外围的一些程序， 另外就是，绝大部分编译不过的问题，都是因为编译器版本的问题，将它强制指定为c++98就好了啊。

--- xapian/build.sh
+++ xapian/build.sh
@@ -6,7 +6,7 @@ then
     tar -xf xapian-core-1.2.13.tar.gz
     cd xapian-core-1.2.13
     mkdir install
-    ./configure --prefix=$PWD/install
+    ./configure --prefix=$PWD/install CXXFLAGS='-std=c++98'
     make -j16
     make install
     cd -
