d91a810dbbb92e9434908b29b055456f74a64084
Allen <njuallen@foxmail.com>
2020-03-22 15:50:34 +0800

    修改了configs.sh，使其在docker中运行。

--- configs.sh
+++ configs.sh
@@ -1,9 +1,9 @@
 # Set this to point to the top level of the TailBench data directory
-DATA_ROOT=~/kernel_interference/tailbench/tailbench.inputs
+DATA_ROOT=/home/kernel_interference/tailbench/tailbench.inputs
 
 # Set this to point to the top level installation directory of the Java
 # Development Kit. Only needed for Specjbb
 JDK_PATH=$JAVA_HOME
 
 # This location is used by applications to store scratch data during execution.
-SCRATCH_DIR=~/kernel_interference/tailbench/tailbench.scratch
+SCRATCH_DIR=/home/kernel_interference/tailbench/tailbench.scratch
